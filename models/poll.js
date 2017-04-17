dc1346a39b95cc56254a48a430dde63cecede878
Nguyen Huy Duc <nmaddp199599@gmail.com>
2017-04-17 16:41:57 +0000

    initial commit

--- models/poll.js
+++ models/poll.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema ;
 var autoIncrement = require('mongoose-auto-increment');
 mongoose.Promise = global.Promise;
 var connection = mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost/myDatabase');
- 
+
 autoIncrement.initialize(connection);
 const pollSchema = new Schema({
   
